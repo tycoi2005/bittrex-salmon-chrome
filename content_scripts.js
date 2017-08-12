c62827635ffee1c72a6c27ce5c67c005a0e2ae86
toanhd <toanhd@cardano-labo.com>
2017-08-12 16:39:38 +0700

    content script

---
+++ content_scripts.js
@@ -0,0 +1,39 @@
+var lotsize = 0.1;
+var isAutoFillLot = true;
+chrome.storage.sync.get({
+    lotsize: 0.1,
+    isAutoFillLot: true,
+  }, function(items) {
+    console.log("loaded item", items)
+    lotsize = items.lotsize;
+    isAutoFillLot = items.isAutoFillLot;
+});
+
+
+$(document).ready(function(){
+/*
+    // not work right now
+    console.log("content ready....")
+    if (isAutoFillLot){
+        console.log("inject lotsize ", lotsize)
+        $('[name=total_Buy]').val(lotsize)
+
+        function injectAsk(){
+            var ask = $($('.market-stats').children()[4]).find('.base-market').find('span')[0].innerText;
+            ask = parseFloat(ask)*1.05; // increase 5%;
+            if (ask>0){
+                console.log("inject ask= ask + 5% == ", ask)
+                $('[name=price_Buy]').val(ask);
+            } else {
+                console.log("injectAsk failed, retrying...")
+                setTimeout(injectAsk, 100)
+            }
+
+            
+        }
+
+        injectAsk();
+        
+    }
+*/
+});
--- options.js
+++ options.js
@@ -7,7 +7,8 @@ function save_options() {
   var smallCoinVolume = document.getElementById('smallCoinVolume').value;
   var bigCoinVolume = document.getElementById('bigCoinVolume').value;
   var isNotifySmallCoin = document.getElementById('isNotifySmallCoin').checked ;
-
+  var lotsize = document.getElementById('lotsize').value;
+  var isAutoFillLot = document.getElementById('isAutoFillLot').checked ;
   if (priceDelta < 0.05) priceDelta = 0.05
   if (tangDelta < 0.05) tangDelta = 0.05
   chrome.storage.sync.set({
@@ -19,6 +20,8 @@ function save_options() {
     smallCoinVolume: smallCoinVolume,
     bigCoinVolume: bigCoinVolume,
     isNotifySmallCoin: isNotifySmallCoin,
+    lotsize: lotsize,
+    isAutoFillLot: isAutoFillLot,
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -42,6 +45,8 @@ function restore_options() {
     smallCoinVolume: 10,
     bigCoinVolume: 1000,
     isNotifySmallCoin: true,
+    lotsize: 0.1,
+    isAutoFillLot: true,
   }, function(items) {
     console.log("loaded item", items)
     document.getElementById('priceDelta').value = items.priceDelta;
@@ -52,6 +57,8 @@ function restore_options() {
     document.getElementById('smallCoinVolume').value = items.smallCoinVolume;
     document.getElementById('bigCoinVolume').value = items.bigCoinVolume;
     document.getElementById('isNotifySmallCoin').checked = items.isNotifySmallCoin;
+    document.getElementById('lotsize').value = items.lotsize;
+    document.getElementById('isAutoFillLot').checked = items.isAutoFillLot;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
